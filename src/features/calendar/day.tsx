f13fe5206a167181186e38d31f5896737f89115f
Senlima <senlima0430@gmail.com>
2023-03-01 04:05:28 +0800

    chore: calendar day arrow key

--- src/features/calendar/day.tsx
+++ src/features/calendar/day.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { format, isSameDay, isSameMonth } from "date-fns"
+import { format, isSameDay, isSameMonth, subDays, addDays } from "date-fns"
 import { Box, Flex, Text } from "@chakra-ui/react"
 
 import { useStore } from "@/store"
@@ -20,12 +20,31 @@ function CalendarDay({ date }: CalendarDayProps) {
     setDate(dateString)
   }
 
+  const onKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    if (e.key === "ArrowLeft") {
+      setDate(format(subDays(date, 1), "yyyy/MM/dd"))
+    }
+    if (e.key === "ArrowRight") {
+      setDate(format(addDays(date, 1), "yyyy/MM/dd"))
+    }
+    if (e.key === "ArrowUp") {
+      setDate(format(subDays(date, 7), "yyyy/MM/dd"))
+    }
+    if (e.key === "ArrowDown") {
+      setDate(format(addDays(date, 7), "yyyy/MM/dd"))
+    }
+    e.currentTarget.blur()
+  }
+
   return (
-    <Box as="td" flex={1} tabIndex={isSelected ? 0 : -1} role="gridcell" pos="relative">
+    <Box as="td" flex={1} tabIndex={isSelected ? 0 : -1} role="gridcell" pos="relative" onKeyDown={onKeyDown}>
       <Flex alignItems="center" justifyContent="center">
         <Text
           role="button"
           fontSize="sm"
+          width="21px"
+          textAlign="center"
           onClick={onDateChange}
           opacity={isSameMonth(date, new Date(currentDate)) ? 1 : 0.5}
           {...(isSelected && {
