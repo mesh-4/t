43b0ba1a637ddcce99a0fa82cefa773839ea8b8b
Senlima <senlima0430@gmail.com>
2023-03-04 21:59:53 +0800

    fix: current day indicator condition update

--- src/features/boxer/index.tsx
+++ src/features/boxer/index.tsx
@@ -4,11 +4,15 @@ import BoxerTimeline from "./timeline"
 import EventsLayer from "./events/layer"
 import EventPlaceholder from "./events/placeholder"
 import CurrentTimeIndicator from "./indicator/current-time"
+import { useStore } from "@/store"
+import { isSameDay } from "date-fns"
 
 function Boxer() {
+  const date = useStore((state) => state.date)
+
   return (
     <EventsLayer>
-      <CurrentTimeIndicator />
+      {isSameDay(new Date(date), new Date()) && <CurrentTimeIndicator />}
       <BoxerTimeline />
       <EventPlaceholder />
     </EventsLayer>
