4dd083465c4ee5aa0228ebadfdba115a15aa3158
Senlima <senlima0430@gmail.com>
2023-08-25 21:22:29 +0800

    styles: make login page able to watch

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,10 +6,10 @@ import SignInForm from "./form"
 
 export default function SignInPage() {
   return (
-    <div className="z-10 mx-auto mt-[calc(30vh)] h-fit w-full max-w-md overflow-hidden border border-gray-100 sm:rounded-2xl sm:shadow-xl">
-      <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white dark:bg-black px-4 py-6 pt-8 text-center sm:px-16">
-        <h3 className="text-xl font-semibold">Sign in to Timebox</h3>
-        <p className="text-sm text-gray-500">Clean task + note for individuals.</p>
+    <div className="z-10 mx-auto mt-[calc(30vh)] h-fit w-full max-w-md overflow-hidden sm:rounded-2xl sm:shadow-xl">
+      <div className="flex flex-col items-center justify-center space-y-3 dark:bg-black px-4 py-6 pt-8 text-center sm:px-16">
+        <h3 className="text-xl font-semibold">Sign in to T</h3>
+        <p className="text-sm text-gray-500">Note, Schedule</p>
       </div>
       <Suspense
         fallback={
